feb338248d3afe2a04b26ebe9e65bf7ed9dbf1b0
Fabián Scherle <fscherle98@gmail.com>
2022-02-13 12:35:33 +0100

    Logo and datasets created

---
+++ app/main_ui2.py
@@ -0,0 +1,17 @@
+import tkinter
+import os.path
+
+# Root of window
+root = tkinter.Tk()
+root.title("OpenSHS")
+# root.resizable(1,1)
+
+root.iconbitmap("logo.ico")
+
+# Window size
+root.geometry("800x800")
+
+# Background colour
+root.config(bg="cyan")
+
+root.mainloop()
