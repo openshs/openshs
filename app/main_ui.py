093a8aa82014d6833668d1b3cf7e9a7b43abe637
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2022-02-14 18:29:28 +0100

    GUI updated

--- app/main_ui.py
+++ app/main_ui.py
@@ -1,37 +1,47 @@
 import PySimpleGUI as sg
 import os.path
 
-# Layout of two columns
-first_column = [
-    [
-        sg.Text("Image Folder"),
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse()
-    ],
-    [
-        sg.Listbox(
-            values=[], enable_events=True, size=(40, 20),
-            key="-FILE LIST-"
-        )
-    ]
+menu_def = [
+    ['Options', ['Generate datasets', 'Tests IA']],
+    ['Tools', ['Replicate datasets', 'Train IA'],],
+    ['Config', ['Enable verbose'],],
+    ['Help', 'About...']
 ]
-
-# Show the name of the selected file
-second_column = [
-    [sg.Text("Choose an image from the list on the left")],
-    [sg.Text(size=(40,1), key="-TOUT-")],
-    [sg.Image(key="-IMAGE-")]
+                
+scenarios = [
+    'morning',
+    'afernoon',
+    'evening',
+    'test1'
+]
+datasets = [
+    'weekday_morning',
+    'weekday_afernoon',
+    'weekday_evening',
+    'weekday_test1'
+]
+ia_mech = [
+    'Lights disable',
+    'Wheater predictor',
+    '...',
+    '...'
 ]
 
 layout = [
+    [sg.Menu(menu_def)],
     [
-        sg.Column(first_column),
-        sg.VSeparator(),
-        sg.Column(second_column)
+        sg.Text('Scenarios:'),
+        sg.Text('\t\t\tDatasets:'),
+        sg.Text('\t\t\tIA Mechanisms:')
+    ],
+    [
+        sg.Listbox(scenarios, size=(30, 20)),
+        sg.Listbox(datasets, size=(30, 20)),
+        sg.Listbox(ia_mech, size=(30, 20))
     ]
 ]
 
-window = sg.Window("Image Viewer", layout)
+window = sg.Window("OpenSHS", layout)
 
 # Event loop:
 while True:
@@ -39,29 +49,5 @@ while True:
     # End program if button is pressed
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
-    # Folder name was filled in
-    if event == "-FOLDER-":
-        folder = values["-FOLDER-"]
-        try:
-            file_list = os.listdir(folder)
-        except:
-            file_list = []
-        fnames = [
-            f
-            for f in file_list
-            if os.path.isfile(os.path.join(folder, f))
-            and f.lower().endswith( (".png", ".gif") )
-        ]
-        window["-FILE LIST-"].update(fnames)
-    # File was chosen
-    elif event == "-FILE LIST-": 
-        try:
-            filename = os.path.join(
-                values["-FOLDER-"], values["-FILE LIST-"][0]
-            )
-            window["-TOUT-"].update(filename)
-            window("-IMAGE-").update(filename=filename)
-        except:
-            pass
 
 window.close()
